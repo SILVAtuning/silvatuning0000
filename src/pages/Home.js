28859e3ffda55d741d9976db676eb8141cc66d94
Kumampet <kumampet@gmail.com>
2024-01-08 15:33:39 +0900

    メニューバー追加。ダークテーマ化

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { DrawerAppBar } from "../components";
+import Toolbar from '@mui/material/Toolbar';
 
 function Home() {
     return (
         <React.Fragment>
+            <DrawerAppBar/>
             <h1>SILVA tuning</h1>
             <p>こんにちは。</p>
         </React.Fragment>
--- src/route/RouterConfig.js
+++ src/route/RouterConfig.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
-    Home
+  Home
 } from "../pages";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
 
 function RouterConfig() {
   return (
     <>
-     <BrowserRouter basename='/silvatuning0000'>
-      <Routes>
-        <Route index element={<Home />} />
-        {/* <Route path="page1" element={<SamplePage1 />} />
+      <ThemeProvider theme={darkTheme}>
+        <BrowserRouter basename='/silvatuning0000'>
+          <Routes>
+            <Route index element={<Home />} />
+            {/* <Route path="page1" element={<SamplePage1 />} />
         <Route path="page2" element={<SamplePage2 />} /> */}
-      </Routes>
-    </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
