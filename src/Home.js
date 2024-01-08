79ae43f1c439e137d9479fea7ee658e51f2bb46e
Kumampet <40848032+Kumampet@users.noreply.github.com>
2024-01-08 12:28:31 +0900

    reactに移行。初期設定 (#1)

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <React.Fragment>
+            <h1>SILVA tuning</h1>
+            <p>こんにちは。</p>
+        </React.Fragment>
+    );
+}
+
+export default Home;
