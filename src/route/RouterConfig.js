4b7ee991bdbbf546ded8e4288d944ca9ba40a367
Kumampet <40848032+Kumampet@users.noreply.github.com>
2024-01-08 14:21:18 +0900

    ルーティング基本設定 (#2)

---
+++ src/route/RouterConfig.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+    Home
+} from "../pages";
+
+
+function RouterConfig() {
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        {/* <Route path="page1" element={<SamplePage1 />} />
+        <Route path="page2" element={<SamplePage2 />} /> */}
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default RouterConfig;
