f740854c422018d3d76e78f4eb6e8fd226c8e3a8
Kumampet <kumampet@gmail.com>
2024-01-12 08:17:28 +0900

    Works末尾にTwitterメディアへのリンク追加

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -5,6 +5,7 @@ import {
   EmbedTable
 } from "../components";
 import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 
 
 function Home() {
@@ -12,11 +13,18 @@ function Home() {
     <React.Fragment>
       <DrawerAppBar />
       <Container maxWidth="md" sx={{ mb: 10 }}>
-        <PageTitle title="Works"/>
+        <PageTitle title="Works" />
         <p>調声させていただいた作品です。</p>
         <p>調声の雰囲気の参考にしていただけますと幸いです。</p>
 
-        <EmbedTable/>
+        <EmbedTable />
+
+        <p>これ以外にもX(Twitter)のメディア欄でも公開しているものもあります。</p>
+        <p>
+          <Link href="https://twitter.com/SILVAVACHAN/media" color="inherit" target="_blank">
+            こちら
+          </Link>からご覧いただけます。
+        </p>
       </Container>
     </React.Fragment>
   );
