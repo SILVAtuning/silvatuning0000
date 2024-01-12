4a192173fe8ee8b594bdd1ad415f2722b6556224
Kumampet <kumampet@gmail.com>
2024-01-12 09:43:14 +0900

    Home画面をリッチにした

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,20 @@ function Home() {
   return (
     <React.Fragment>
       <DrawerAppBar />
-      <h1>SILVA tuning</h1>
-      <p>こんにちは。</p>
+      <div className="home-bg">
+        <video
+          autoPlay
+          loop
+          muted
+        >
+          <source
+            src={`${process.env.PUBLIC_URL}/video/silva_tuning_opening.mp4`}
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      {/* <h1>SILVA tuning</h1>
+      <p>こんにちは。</p> */}
     </React.Fragment>
   );
 }
