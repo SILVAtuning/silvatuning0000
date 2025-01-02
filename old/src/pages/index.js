65e0dcec7b4b597ea641db9484c7a17faa98b615
Kumampet <kumampet@gmail.com>
2025-01-02 16:37:39 +0900

    古いJavascriptで作成されたプロジェクトを削除

--- old/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home';
-export { default as Works } from './Works';
-export { default as About } from './About';
-export { default as Pricement } from './Pricement';
