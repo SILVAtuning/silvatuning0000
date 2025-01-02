399699a9bf4081d34b56210e8cd56fcfa97e39eb
Kumampet <kumampet@gmail.com>
2025-01-02 19:02:15 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,13 @@ const nextConfig: NextConfig = {
     includePaths: [path.join(__dirname, "styles")],
     implementation: 'sass-embedded',
   },
+  env: {
+    NEXT_PUBLIC_REACT_APP_GOOGLE_SHEETS_API_KEY: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_SHEETS_API_KEY,
+    NEXT_PUBLIC_REACT_APP_GOOGLE_SHEETS_DOC_ID: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_SHEETS_DOC_ID,
+    NEXT_PUBLIC_REACT_APP_MAIL_ADDRESS: process.env.NEXT_PUBLIC_REACT_APP_MAIL_ADDRESS,
+    NEXT_PUBLIC_REACT_APP_X_ACCOUNT_ID: process.env.NEXT_PUBLIC_REACT_APP_X_ACCOUNT_ID,
+    NEXT_PUBLIC_REACT_APP_SITE_NAME: process.env.NEXT_PUBLIC_REACT_APP_SITE_NAME
+  }
 };
 
 export default nextConfig;
