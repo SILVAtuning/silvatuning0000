cb7cbca5a31d37b18e3afe244d4b15ee2a7b1157
Kumampet <kumampet@gmail.com>
2025-01-03 13:32:29 +0900

    basePath設定

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const env = process.env.NODE_ENV;
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,7 +11,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: "",
+  basePath: env === "production" ? "/silvatuning0000" : "",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     implementation: 'sass-embedded',
