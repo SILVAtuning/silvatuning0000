631fc54eb183869de64b1faa378a99066beaa5d4
Kumampet <kumampet@gmail.com>
2025-01-03 15:29:10 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const env = process.env.NEXT_PUBLIC_NODE_ENV;
+const env = process.env.NODE_ENV;
+console.log({ env })
 
 const nextConfig: NextConfig = {
   /* config options here */
