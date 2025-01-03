9f8ba2b30c1f1877f7355f2b1e9fb45d86cf7432
Kumampet <kumampet@gmail.com>
2025-01-03 15:56:51 +0900

    fix

--- src/utils/getBasePath.ts
+++ src/utils/getBasePath.ts
@@ -1,5 +1,5 @@
 import _get from 'lodash/get';
 
 export default function getBasePath() {
-  return _get(process.env, 'NEXT_PUBLIC_BASE_PATH', '');
+  return _get(process.env, 'NEXT_PUBLIC_REACT_APP_BASE_PATH', '');
 }
