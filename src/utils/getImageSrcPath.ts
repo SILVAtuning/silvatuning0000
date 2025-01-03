cf7e6bd807d1ec152fe00d84f10208a8ba00c91b
Kumampet <kumampet@gmail.com>
2025-01-03 16:20:33 +0900

    fix

--- src/utils/getImageSrcPath.ts
+++ src/utils/getImageSrcPath.ts
@@ -10,7 +10,8 @@ const getImageSrcPath = (src: string) => {
   if (_first(src) !== '/') {
     src = '/' + src;
   }
-  return `${getBasePath()}${src}`;
+  const base = process.env.NODE_ENV === "development" ? "" : "/silvatuning0000"
+  return `${getBasePath()}/${base}${src}`;
 }
 
 export default getImageSrcPath;
